0348dfddca70d44ac5bfa3dae1a66220336cb264
Dom-Kub <82820260+Dom-Kub@users.noreply.github.com>
2022-02-05 18:05:31 +0100

    Update MovieRepositroy.java

--- src/main/java/sk/dominikkubala/learntocode/druhyrepository/MovieRepositroy.java
+++ src/main/java/sk/dominikkubala/learntocode/druhyrepository/MovieRepositroy.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import sk.dominikkubala.learntocode.prvymodel.Movie;
 
 public interface MovieRepositroy extends CrudRepository<Movie,Long> {
-  // skuska pullnutia
+ 
 
 }
